00277cf346256a2873244823cd94c1bf641f035d
Sreepati <sreepati@jigsawacademy.com>
2019-01-31 17:46:37 +0530

    timeline started

--- src/component/timeline.component.jsx
+++ src/component/timeline.component.jsx
@@ -4,10 +4,25 @@ export class Timeline extends Component {
 
     render() {
         return (
-            <div className="body">
+            <div className="body timeline">
                 <div className="content">
-                    <h3>timeline</h3>
-                    <p>timeline</p>
+                    <h3>Timeline</h3>
+                    <div className="timeline-block">
+                        <p className="year">2019 <span className="month">January</span></p>
+                        <p className="desc">Released Visual Studio Code extension.</p>
+                    </div>
+                    <div className="timeline-block">
+                        <p className="year">2017 <span className="month">November</span></p>
+                        <p className="desc">Married</p>
+                    </div>
+                    <div className="timeline-block">
+                        <p className="year">2016 <span className="month">July</span></p>
+                        <p className="desc">Joined Jigsaw Academy Education Pvt. Ltd. as PHP Developer</p>
+                    </div>
+                    <div className="timeline-block">
+                        <p className="year">2013 <span className="month">April</span></p>
+                        <p className="desc">Joined Synapse India Pvt. Ltd. as Software Engineer</p>
+                    </div>
                 </div>
             </div >
         )
