c93356502e864f1ccaad5e108da5703f27d2b66c
Sreepati <sreepati@jigsawacademy.com>
2019-02-01 14:22:36 +0530

    timeline v 1

--- src/component/timeline.component.jsx
+++ src/component/timeline.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import '../timeline.css';
 export class Timeline extends Component {
 
     render() {
@@ -7,21 +7,24 @@ export class Timeline extends Component {
             <div className="body timeline">
                 <div className="content">
                     <h3>Timeline</h3>
-                    <div className="timeline-block">
-                        <p className="year">2019 <span className="month">January</span></p>
-                        <p className="desc">Released Visual Studio Code extension.</p>
-                    </div>
-                    <div className="timeline-block">
-                        <p className="year">2017 <span className="month">November</span></p>
-                        <p className="desc">Married</p>
-                    </div>
-                    <div className="timeline-block">
-                        <p className="year">2016 <span className="month">July</span></p>
-                        <p className="desc">Joined Jigsaw Academy Education Pvt. Ltd. as PHP Developer</p>
-                    </div>
-                    <div className="timeline-block">
-                        <p className="year">2013 <span className="month">April</span></p>
-                        <p className="desc">Joined Synapse India Pvt. Ltd. as Software Engineer</p>
+                    <div className="timeline-list">
+                        <span className="line"><span className="line-covered"></span></span>
+                        <div className="timeline-block odd first">
+                            <p className="year">2019 <span className="month">January</span></p>
+                            <p className="desc">Released Visual Studio Code extension.</p>
+                        </div>
+                        <div className="timeline-block even second">
+                            <p className="year">2017 <span className="month">November</span></p>
+                            <p className="desc">Married</p>
+                        </div>
+                        <div className="timeline-block odd third">
+                            <p className="year">2016 <span className="month">July</span></p>
+                            <p className="desc">Joined Jigsaw Academy Education Pvt. Ltd. as PHP Developer</p>
+                        </div>
+                        <div className="timeline-block even fourth">
+                            <p className="year">2013 <span className="month">April</span></p>
+                            <p className="desc">Joined Synapse India Pvt. Ltd. as Software Engineer</p>
+                        </div>
                     </div>
                 </div>
             </div >
